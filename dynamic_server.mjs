3040bb96e3343f30bbf5b08049dee6bcc67a057d
Rafay Malik <mali4211@ssthomas.edu>
2023-11-02 18:23:26 -0500

    Rafay Update

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -1,72 +1,107 @@
 import * as path from 'node:path';
+import * as fs from 'node:fs';
 import * as url from 'node:url';
-
-
 import { default as express } from 'express';
 import { default as sqlite3 } from 'sqlite3';
 
-const port = 8000;
+const port = 8088;
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const root = path.join(__dirname, 'templates');
-const template = path.join(__dirname, 'templates');
+const root = path.join(__dirname, 'public');
+const templates = path.join(__dirname, 'templates');
 
-let app = express();
+const app = express();
 app.use(express.static(root));
 
-
-
 const db = new sqlite3.Database(path.join(__dirname, 'data.sqlite3'), sqlite3.OPEN_READONLY, (err) => {
-    if (err) {console.log('Error connecting to database');}
-    else {console.log('Successfully connected to database');}
+    if (err) {
+        console.log('Error connecting to the database');
+    } else {
+        console.log('Successfully connected to the database');
+    }
 });
 
-
 function dbSelect(query, params) {
-    let p = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         db.all(query, params, (err, rows) => {
-            if(err){
+            if (err) {
                 reject(err);
-            }
-            else{
+            } else {
                 resolve(rows);
             }
         });
     });
-    return p;
 }
-
-
-app.get('/:name', (req, res) => {
-    let region = req.params.name.toUpperCase();
+app.get('/year/:year', (req, res) => {
+    fs.promises.readFile(path.join(templates, 'temp1.html'), 'utf-8').then((template) => {
+        res.status(200).type('html').send(template);
+    }).catch((err) => {
+        res.status(404).type('txt').send('File Not Found');
+    });
+});
+app.get('/:name', async (req, res) => {
+    const region = req.params.name.toUpperCase();
     console.log(region);
 
-    let query1 = 'SELECT * FROM Cereals WHERE mfr = ?';
-    let query2 = 'SELECT * FROM Manufacturers WHERE id = ?';
-    let p1 = dbSelect(query1, [region]);
-    let p2 = dbSelect(query2, [region]);
-    Promise.all([p1, p2]).then((results) => {
-        let response = results[2].replace('$$', results[1][0].name);
-        let table_body = '';
-        results[0].forEach((cereal) => {
-            let table_row = '<tr>';
-            table_row += '<td>' + cereal.name + '</td>';
-            table_row += '<td>' + cereal.type + '</td>';
-            table_row += '<td>' + cereal.calories + '</td>';
-            table_row += '<td>' + cereal.fat + '</td>';
-            table_row += '<td>' + cereal.protein + '</td>';
-            table_row += '<td>' + cereal.carbohydrates + '</td>';
-            table_row += '</tr>\n';
-            table_body += table_row;
-        });
-    }).catch((error) => {
-        res.status(404).type('txt').send("File Not Found");
-    })
+    const query1 = 'SELECT * FROM Cereals WHERE mfr = ?';
+    const query2 = 'SELECT * FROM Manufacturers WHERE id = ?';
+
+    try {
+        const [cereals, manufacturer] = await Promise.all([dbSelect(query1, [region]), dbSelect(query2, [region])]);
+        
+        const response = manufacturer[0].name;
+
+        res.send(`
+            <html>
+            <head>
+                <title>${response}</title>
+            </head>
+            <body>
+                <h1>${response}</h1>
+                <table>
+                    <tr><th>Name</th><th>Type</th><th>Calories</th><th>Fat</th><th>Protein</th><th>Carbohydrates</th></tr>
+                    ${cereals.map(cereal => `
+                        <tr>
+                            <td>${cereal.name}</td>
+                            <td>${cereal.type}</td>
+                            <td>${cereal.calories}</td>
+                            <td>${cereal.fat}</td>
+                            <td>${cereal.protein}</td>
+                            <td>${cereal.carbohydrates}</td>
+                        </tr>`).join('')}
+                </table>
+                <div id="chart-container">
+                    <canvas id="myChart"></canvas>
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <script>
+                    const labels = ['Calories', 'Fat', 'Protein', 'Carbohydrates'];
+                    const data = [${cereals[0].calories}, ${cereals[0].fat}, ${cereals[0].protein}, ${cereals[0].carbohydrates}];
+
+                    const ctx = document.getElementById('myChart').getContext('2d');
+                    new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: 'Cereal Data',
+                                data: data,
+                                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                                borderColor: 'rgba(75, 192, 192, 1)',
+                                borderWidth: 1,
+                            }],
+                        },
+                    });
+                </script>
+            </body>
+            </html>
+        `);
+    } catch (error) {
+        res.status(404).type('txt').send('File Not Found');
+    }
 });
-
-
-
-
 
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 });
+
+
